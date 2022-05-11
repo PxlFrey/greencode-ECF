c8c0eee01ccfe1e7a9a9f8676dbfe1ce8db2977e
PxlFrey <fred.chretien@outlook.fr>
2022-05-11 14:48:29 +0200

    Added verification of password in forms

--- public/assets/js/greencode.js
+++ public/assets/js/greencode.js
@@ -1,20 +1,46 @@
+// Rendre visible le mot de passe pour vérification
+$(function() {
+    const togglePassword = document.querySelector('#togglePassword');
+    const password = document.querySelector('#registration_form_plainPassword');
+    const postulate_pw = document.querySelector('#coach_form_plainPassword');
 
-// Filtre de recherche à la page formations
-
-  // Capture mes cartes de formations dans une constante
-  const cards = document.querySelectorAll('.card');
-  // Capture le champ de recherche
-  const searchCard = document.querySelector('#search');
-
-  // Fonction de filtre de recherche sur le titre de la formation
-  searchCard.addEventListener('input', () => {
-    const searchValue = searchCard.value.toLowerCase();
-    cards.forEach(card => {
-      const cardTitle = card.querySelector('.title-card').innerText.toLowerCase();
-      if (cardTitle.includes(searchValue)) {
-        card.style.display = 'block';
+    // Pour la page /s'inscrire
+    togglePassword.addEventListener('click', () => {
+      if (password.type === 'password') {
+        password.type = 'text';
       } else {
-        card.style.display = 'none';
-      }
+        password.type = 'password';
+      }      
+    });
+
+    // Pour la page /postuler
+    togglePassword.addEventListener('click', () => {
+      if (postulate_pw.type === 'password') {
+        postulate_pw.type = 'text';
+      } else {
+        postulate_pw.type = 'password';
+      }      
+    });
+});
+
+// Filtre de recherche à la page /formations
+$(function() {
+  // Capture mes cartes de formations dans une constante
+    const cards = document.querySelectorAll('.card');
+
+  // Capture le champ de recherche
+    const searchCard = document.querySelector('#search');
+    
+  // Fonction de filtre de recherche sur le titre de la formation
+    searchCard.addEventListener('input', () => {
+      const searchValue = searchCard.value.toLowerCase();
+      cards.forEach(card => {
+        const cardTitle = card.querySelector('.title-card').innerText.toLowerCase();
+        if (cardTitle.includes(searchValue)) {
+          card.style.display = 'block';
+        } else {
+          card.style.display = 'none';
+        }
+      });
     });
 });
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,11 +6,14 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+
+use function PHPSTORM_META\type;
 
 class RegistrationFormType extends AbstractType
 {
@@ -29,19 +32,20 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+                
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'help' => 'Veuillez entrer un mot de passe de plus de 6 caractères.',
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Veuillez renseigner un mot de passe',
-                    ]),
+                   ]),
                     new Length([
                         'min' => 6,
                         'minMessage' => 'Votre mot de passe doit contenir minimum {{ limit }} caractères',
-                        // max length allowed by Symfony for security reasons
+                       // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
                 ],
